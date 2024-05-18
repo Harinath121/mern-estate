96b01718d3c84475c5f8c1d4252fdb0bb0537bfc
Harinath121 <hariharinath121@gmail.com>
2024-05-19 04:08:38 +0530

    final complete project.

--- client/src/Pages/About.jsx
+++ client/src/Pages/About.jsx
@@ -2,6 +2,16 @@ import React from 'react'
 
 export default function About() {
   return (
-    <div>About</div>
+    <div className='py-20 px-4 max-w-6xl mx-auto'>
+      <h1 className="text-3xl font-bold mb-4">About Ruth Estate</h1>
+      <p className="">
+      Ruth Estate is a premier rental and property sale website dedicated to helping you find your dream home.
+      Whether you're looking to rent or buy, our extensive listings feature a wide range of properties to suit every budget and lifestyle.
+      With user-friendly search options, detailed property descriptions, and high-quality images, Ruth Estate makes it easy to explore and compare options.
+      Our commitment to excellence ensures a seamless experience, from browsing to closing. Trust Ruth Estate to provide expert guidance and support in your
+      journey to find the perfect place to call home.
+      </p>
+      
+    </div>
   )
 }
